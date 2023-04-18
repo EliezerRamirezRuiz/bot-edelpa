e48b265806e91c2a18d91810f685ac8eefc1248e
Eli-ezer Ramirez Ruiz <eramirezr@edelpa.cl>
2023-04-18 17:39:46 -0400

    add function with procedure bbdd

--- src/Database/Clases/automic.py
+++ src/Database/Clases/automic.py
@@ -2,10 +2,10 @@ import discord
 import asyncio
 
 from Database.db import Database
-
+ 
 
 class AutomaticAlerta(Database):
-    async def get_data(self):
+    async def get_data(self) -> list:
         """ Funcion para traer datos de la base de datos SQL Server, 
         para ser mas exacto muchas alertas, todo a traves de un 
         procedimiento almacenado"""
@@ -17,6 +17,7 @@ class AutomaticAlerta(Database):
                 await cursor.execute(query)
                 row = await cursor.fetchall()
 
+                
                 print(row)
                 return row
 
@@ -27,13 +28,30 @@ class AutomaticAlerta(Database):
             await cursor.close()
             await conn.close()
 
+
+    async def cambiar_estado_alerta(self, code) -> None:
+        """Funcion que executa un procedimiento almacenado que apaga las alarmas,
+        se executara despues de haber mandado las alertas con la funcion `self.auto_alertas()`"""
+        try:
+            conn = await self.connection_sqlserver()
+            async with conn.cursor() as cursor:
+                query = f" EXEC APAGAR_ALERTAS {code}"
+                await cursor.execute(query)
+
+        except Exception as ex:
+            print(f' Apagar alertas tiene un error en :{ex}')
+
+        finally:
+            await cursor.close()
+            await conn.close()
+
     
-    async def auto_alertas(self, bot):
+    async def auto_alertas(self, bot) -> None:
         """Cada cierto tiempo trae alertas y las muestra de forma automatica para
         que los usuarios se den cuenta que hay un error en el robot o alguna area"""
 
         while True:
-            await asyncio.sleep(60)
+            await asyncio.sleep(1)
             lista_alertas = await self.get_data()
 
             if len(lista_alertas) == 0:
@@ -53,3 +71,4 @@ class AutomaticAlerta(Database):
                     embed.set_image(url='https://raw.githubusercontent.com/EliezerEdelpa/Imagenes-Edelpa/main/foto_edelpa.png')
 
                     await channel.send(embed=embed)
+                    await self.cambiar_estado_alerta(data[2])
